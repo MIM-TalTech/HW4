f97df9f8905ad4829f1c69f07a911a95b8c20da9
Markus Ilmar Münzer <mamunz@ttu.ee>
2020-03-09 23:52:25 +0200

    HW6-P8

--- Soft/Program.cs
+++ Soft/Program.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HW4.Infra.Quantity;
+using HW4.Pages.Quantity;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +16,14 @@ namespace Soft
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var dbQuantity = services.GetRequiredService<QuantityDbContext>();
+                QuantityDbInitializer.Initialize(dbQuantity);
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
