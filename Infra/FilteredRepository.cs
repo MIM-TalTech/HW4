70ae3e2b9c44591f802cffc4a924ac4423298df5
Markus Ilmar Münzer <mamunz@ttu.ee>
2020-03-10 15:59:46 +0200

    HW6-P9

--- Infra/FilteredRepository.cs
+++ Infra/FilteredRepository.cs
@@ -1,10 +1,12 @@
-﻿using HW4.Data.Common;
+﻿using HW4.Data;
+using HW4.Data.Common;
 using HW4.Domain;
 using HW4.Domain.Quantity;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace HW4.Infra
@@ -13,6 +15,8 @@ namespace HW4.Infra
          where TData : UniqueEntityData, new()
         where TDomain : Entity<TData>, new()
     {
+         Expression predicate = null;
+
         protected FilteredRepository(DbContext c, DbSet<TData> s) : base(c, s)
         {
             
@@ -24,10 +28,34 @@ namespace HW4.Infra
             return query;
         }
 
-        protected internal virtual IQueryable<TData> addFiltering(IQueryable<TData> query)
+        
+        protected internal IQueryable<TData> addFiltering(IQueryable<TData> query)
         {
-            return query;
+            if (string.IsNullOrEmpty(SearchString)) return query;
+            var expression = createWhereExpression();
+            
+            return query.Where(expression);
+
         }
+
+        internal Expression<Func<TData, bool>> createWhereExpression()
+        {
+            var param = Expression.Parameter(typeof(TData), "s");
+            foreach(var p in typeof(TData).GetProperties())
+            {
+                Expression body = Expression.Property(param, p);
+                if (p.PropertyType != typeof(string))
+                    body = Expression.Call(body, "ToString", null);
+                    body = Expression.Call(body, "Contains", null, Expression.Constant(SearchString));
+
+                if (predicate is null) predicate = body;
+                else predicate = Expression.Or(predicate, body);
+
+            }
+            if (predicate is null) return null;
+            return Expression.Lambda<Func<TData, bool>>(predicate, param);
+        }
+        
 
         public string SearchString { get; set; }
     }
--- Infra/Quantity/MeasuresRepository.cs
+++ Infra/Quantity/MeasuresRepository.cs
@@ -21,23 +21,6 @@ namespace Infra
         {
             return await dbSet.FirstOrDefaultAsync(m => m.Id == id);
         }
-
-        protected internal override IQueryable<MeasureData> addFiltering(IQueryable<MeasureData> set)
-        {
-            if (string.IsNullOrEmpty(SearchString)) return set;
-            
-                return set.Where(s => s.Name.Contains(SearchString)
-                                       || s.Code.Contains(SearchString)
-                                       || s.Id.Contains(SearchString)
-                                       || s.Definition.Contains(SearchString)
-                                       || s.ValidFrom.ToString().Contains(SearchString)
-                                       || s.ValidTo.ToString().Contains(SearchString));
-            
-        }
-
-       
-     
-
         protected internal override Measure toDomainObject(MeasureData d)
         {
             return new Measure(d);
