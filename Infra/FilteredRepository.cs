6c6641b4f773d90e663e140e64da7081f353b840
Markus Ilmar Münzer <mamunz@ttu.ee>
2020-04-01 05:46:42 +0300

    HW9-P7

--- Infra/FilteredRepository.cs
+++ Infra/FilteredRepository.cs
@@ -29,17 +29,17 @@ namespace HW4.Infra
             return query;
         }
 
-        private IQueryable<TData> addFixedFiltering(IQueryable<TData> query)
+        internal IQueryable<TData> addFixedFiltering(IQueryable<TData> query)
         {
             var expression = createFixedWhereExpression();
             return expression is null? query :
             query.Where(expression);
         }
 
-        private Expression<Func<TData, bool>>createFixedWhereExpression()
+        internal Expression<Func<TData, bool>>createFixedWhereExpression()
         {
-            if (FixedFilter is null) return null;
-            if (FixedValue is null) return null;
+            if (string.IsNullOrWhiteSpace(FixedValue)) return null;
+            if (string.IsNullOrWhiteSpace(FixedFilter)) return null;
             var param = Expression.Parameter(typeof(TData), "s");
           
             var p = typeof(TData).GetProperty(FixedFilter);
@@ -63,6 +63,7 @@ namespace HW4.Infra
 
         internal Expression<Func<TData, bool>> createWhereExpression()
         {
+            if (string.IsNullOrWhiteSpace((SearchString))) return null;
             var param = Expression.Parameter(typeof(TData), "s");
             Expression predicate = null;
             foreach (var p in typeof(TData).GetProperties())
---
+++ Tests/Infra/Quantity/UniqueEntityRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HW4.Tests.Infra.Quantity
+{
+    [TestClass]
+    public class UniqueEntityRepositoryTests
+    {
+    }
+}
